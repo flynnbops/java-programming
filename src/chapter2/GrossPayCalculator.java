95f9fa0c96b92573276b3ce5c53d0f53470f333c
Aaron Flynn <aaron.flynn@digital.landregistry.gov.uk>
2020-11-24 18:54:13 +0000

    chapter 2 first exercise

---
+++ src/chapter2/GrossPayCalculator.java
@@ -0,0 +1,23 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+    public static void main(String[] args){
+
+        // 1. Get number of hours worked
+        System.out.println("How many hours did you work this week?");
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+
+        // 2. Get hourly rate
+        System.out.println("Enter your hourly rate in £");
+        double pay = scanner.nextDouble();
+
+        // 3. Multiply 1 by 2
+        double grossPay = hours * pay;
+
+        // 4. Display the result
+        System.out.println("Your gross pay is calculated as: £" + grossPay);
+    }
+}
