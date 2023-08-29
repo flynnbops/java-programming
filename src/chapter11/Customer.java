449475c89621dc45cc86d39b6a47a8b88dbfdb80
Aaron Flynn <aaron.flynn@brightpearl.com>
2023-08-29 16:31:31 +0100

    Chapter 11b Interfaces

---
+++ src/chapter11/Customer.java
@@ -0,0 +1,14 @@
+package chapter11;
+
+public class Customer {
+  public static void main(String[] args){
+
+    Product book = new Book();
+
+    book.setName("Hiya");
+    book.setPrice(9.99);
+    book.setColour("Red");
+
+    Book book2= new Book();
+  }
+}
